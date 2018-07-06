d4e82280c2e37db38e78574cbfc8b39acf5c8a11
Naitree Zhu <naitreey@gmail.com>
2018-07-06 17:40:35 +0800

    add unique together constraint and tests

--- todo/tests/test_models.py
+++ todo/tests/test_models.py
@@ -6,34 +6,21 @@ from django.core.exceptions import NON_FIELD_ERRORS
 from .. import models
 
 
-class ListAndItemModelTest(TestCase):
+class ItemModelTest(TestCase):
 
-    def test_saving_and_get_items(self):
+    def test_item_belongs_to_list(self):
         todo_list = models.List.objects.create()
         item1_text = "new item1"
-        item2_text = "new item2"
         item1 = models.Item(
             content=item1_text,
             create_time=timezone.now(),
             list=todo_list,
         )
         item1.save()
-        item2 = models.Item(
-            content=item2_text,
-            create_time=timezone.now(),
-            list=todo_list
-        )
-        item2.save()
-        self.assertEqual(models.Item.objects.count(), 2)
         self.assertEqual(
-            models.Item.objects.get(content=item1_text).content,
-            item1.content
+            item1,
+            todo_list.entries.get()
         )
-        self.assertEqual(
-            models.Item.objects.get(content=item2_text).content,
-            item2.content
-        )
-        self.assertEqual(todo_list.entries.count(), 2)
 
     def test_item_can_not_be_empty(self):
         todo_list = models.List.objects.create()
@@ -47,13 +34,6 @@ class ListAndItemModelTest(TestCase):
         self.assertIn(
             "This field cannot be blank.",
             cm.exception.message_dict['content']
-        )
-
-    def test_list_url(self):
-        todo_list = models.List.objects.create()
-        self.assertEqual(
-            todo_list.get_absolute_url(),
-            f"/lists/{todo_list.pk}/"
         )
 
     def test_can_not_add_same_item_to_a_list(self):
@@ -76,3 +56,13 @@ class ListAndItemModelTest(TestCase):
         item2 = models.Item(content=text, list=list2)
         item1.full_clean() # should not raise
         item2.full_clean() # should not raise
+
+
+class ListModelTest(TestCase):
+
+    def test_list_url(self):
+        todo_list = models.List.objects.create()
+        self.assertEqual(
+            todo_list.get_absolute_url(),
+            f"/lists/{todo_list.pk}/"
+        )
